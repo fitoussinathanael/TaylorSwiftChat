d4477b1be6de6587a38f72c12bdb04e852057055
fitoussinathanael <fitoussi.nathanael@gmail.com>
2026-04-28 21:49:56 +0200

    Update rag_icu.py

--- rag_icu.py
+++ rag_icu.py
@@ -1,10 +1,91 @@
 ICU_DATABASE = {
+
     "midazolam": {
         "classe": "benzodiazépine",
-        "usage": "sédation ICU",
-        "effets": "dépression respiratoire",
+        "usage": "sédation ICU, agitation, intubation",
+        "effets": "dépression respiratoire, hypotension",
         "surveillance": "TA, FR, RASS",
-        "points_icu": "accumulation"
+        "points_icu": "accumulation en perfusion prolongée, sevrage possible"
+    },
+
+    "propofol": {
+        "classe": "hypnotique IV",
+        "usage": "sédation patient ventilé",
+        "effets": "hypotension, bradycardie",
+        "surveillance": "TA, TG, sédation",
+        "points_icu": "risque syndrome perfusion propofol"
+    },
+
+    "fentanyl": {
+        "classe": "opioïde",
+        "usage": "analgésie en réanimation",
+        "effets": "dépression respiratoire, bradycardie",
+        "surveillance": "FR, douleur, sédation",
+        "points_icu": "accumulation possible"
+    },
+
+    "morphine": {
+        "classe": "opioïde",
+        "usage": "analgésie",
+        "effets": "dépression respiratoire, hypotension",
+        "surveillance": "FR, douleur, conscience",
+        "points_icu": "attention insuffisance rénale"
+    },
+
+    "noradrenaline": {
+        "classe": "vasopresseur",
+        "usage": "choc septique, hypotension",
+        "effets": "vasoconstriction",
+        "surveillance": "TA invasive",
+        "points_icu": "voie centrale recommandée, extravasation"
+    },
+
+    "dobutamine": {
+        "classe": "inotrope",
+        "usage": "choc cardiogénique",
+        "effets": "tachycardie",
+        "surveillance": "FC, TA",
+        "points_icu": "risque arythmie"
+    },
+
+    "adrenaline": {
+        "classe": "catécholamine",
+        "usage": "arrêt cardiaque, choc",
+        "effets": "tachycardie, HTA",
+        "surveillance": "ECG, TA",
+        "points_icu": "risque arythmie"
+    },
+
+    "ceftriaxone": {
+        "classe": "antibiotique",
+        "usage": "infection sévère",
+        "effets": "allergie",
+        "surveillance": "clinique, biologique",
+        "points_icu": "ne pas associer calcium IV"
+    },
+
+    "piperacilline": {
+        "classe": "antibiotique",
+        "usage": "infection nosocomiale",
+        "effets": "allergie",
+        "surveillance": "clinique",
+        "points_icu": "adapter fonction rénale"
+    },
+
+    "potassium": {
+        "classe": "électrolyte",
+        "usage": "hypokaliémie",
+        "effets": "troubles du rythme",
+        "surveillance": "ECG, kaliémie",
+        "points_icu": "perf lente, voie sécurisée"
+    },
+
+    "calcium": {
+        "classe": "électrolyte",
+        "usage": "hypocalcémie",
+        "effets": "arythmie",
+        "surveillance": "ECG, calcémie",
+        "points_icu": "attention incompatibilité ceftriaxone"
     }
 }
 
